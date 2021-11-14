7a49cf17eab5eeeb03e126f90e0d76cdf94161a6
Sceglia2 <>
2021-11-14 16:36:28 +0100

    init node

---
+++ SEProject_Team20/src/it/unisa/seProjectWork/team20/NewClass.java
@@ -0,0 +1,13 @@
+package it.unisa.seProjectWork.team20;
+
+/**
+ *
+ * @author Team 20
+ */
+public class NewClass {
+    
+    public static void main(String args[]){
+        System.out.println("Hello, world!");
+    }
+    
+}
