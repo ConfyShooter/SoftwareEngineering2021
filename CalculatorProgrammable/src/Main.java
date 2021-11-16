3ebe533d72d2d2c15728f6e8a95d26e0e1067cd1
Sceglia2 <Admin@DESKTOP-8BC5J3V.station>
2021-11-16 16:41:13 +0100

    Salvatore test 2

---
+++ CalculatorProgrammable/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Alessandro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ci siamo");
+        System.out.println("TEST_1 git");
+        System.out.println("");
+    }
+
+}
