844501b677dbd52f4e62df4bfdd135954ccb31a3
Sceglia2 <Admin@DESKTOP-8BC5J3V.station>
2021-11-16 17:06:55 +0100

    Salvatore 1

---
+++ CalculatorProgrammable/src/it/unisa/diem/SE2021/GUI/GUIMain.java
@@ -0,0 +1,37 @@
+package it.unisa.diem.SE2021.GUI;
+
+import java.io.IOException;
+import javafx.application.*;
+import javafx.beans.binding.*;
+import javafx.beans.property.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.fxml.*;
+/**
+*
+* @author Gruppo 20
+*/
+public class GUIMain extends Application {
+
+
+
+@Override
+public void start(Stage stage) throws IOException {
+Parent root = FXMLLoader.load(getClass().getResource("GUI_FXML.fxml"));
+Scene scene = new Scene(root, 800, 600);
+stage.setTitle("Hello World!");
+stage.setScene(scene);
+stage.show();
+}
+
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+launch(args);
+}
+
+}
---
+++ CalculatorProgrammable/src/it/unisa/diem/SE2021/GUI/GUI_FXMLController.java
@@ -0,0 +1,22 @@
+package it.unisa.diem.SE2021.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gruppo 20
+ */
+public class GUI_FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
