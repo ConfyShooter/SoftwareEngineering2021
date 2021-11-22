96a7442c5cf6827e83293642ea2c8d247b77df14
Alessandro <Alessandro>
2021-11-22 16:38:12 +0100

    Created empty source file with methods' signatures.

--- CalculatorProgrammable/src/it/unisa/diem/SE2021/Test/UserDefinedOperationsTest.java
+++ CalculatorProgrammable/src/it/unisa/diem/SE2021/Test/UserDefinedOperationsTest.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package it.unisa.diem.SE2021.Test;
 
 /**
  *
- * @author Alessandro
+ * @author Gruppo20
  */
 public class UserDefinedOperationsTest {
     
