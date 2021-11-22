d09d0fe80dc24339f0df24c1be0430e48c925976
Alessandro <Alessandro>
2021-11-22 16:36:58 +0100

    Created empty source file with methods' signatures.

--- CalculatorProgrammable/src/it/unisa/diem/SE2021/Model/Calculator.java
+++ CalculatorProgrammable/src/it/unisa/diem/SE2021/Model/Calculator.java
@@ -6,23 +6,125 @@ package it.unisa.diem.SE2021.Model;
  */
 public interface Calculator {
     
+    /**
+     *
+     * @param number
+     * @return
+     */
     public boolean insert(String number);
+
+    /**
+     *
+     * @return
+     */
     public boolean sum();
+
+    /**
+     *
+     * @return
+     */
     public boolean subtract();
+
+    /**
+     *
+     * @return
+     */
     public boolean multiply();
+
+    /**
+     *
+     * @return
+     */
     public boolean division();
+
+    /**
+     *
+     * @return
+     */
     public boolean sqrt();
+
+    /**
+     *
+     * @return
+     */
     public boolean invertSign();
+
+    /**
+     *
+     * @return
+     */
     public boolean clear();
+
+    /**
+     *
+     * @return
+     */
     public boolean drop();
+
+    /**
+     *
+     * @return
+     */
     public boolean dup();
+
+    /**
+     *
+     * @return
+     */
     public boolean over();
+
+    /**
+     *
+     * @param c
+     * @return
+     */
     public boolean saveIntoVariable(char c);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
     public boolean saveFromVariable(char c);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
     public boolean sumWithVariable(char c);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
     public boolean subtractWithVariable(char c);
+
+    /**
+     *
+     * @return
+     */
     public boolean saveVariables();
+
+    /**
+     *
+     * @return
+     */
     public boolean restoreVariables();
+
+    /**
+     *
+     * @param name
+     * @return
+     */
     public boolean executeOperation(String name);
+
+    /**
+     *
+     * @param name
+     * @param op
+     * @return
+     */
     public boolean addOperation(String name,String[] op);
 }
