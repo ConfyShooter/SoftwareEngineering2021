2ef82a0fe4048be669763d942794d07cbfaa3e7b
Alessandro <Alessandro@192.168.1.103>
2021-11-30 14:53:25 +0100

    Added JavaDoc to Command

--- ProgrammableCalculator/src/it/unisa/diem/Gruppo20/Model/Command.java
+++ ProgrammableCalculator/src/it/unisa/diem/Gruppo20/Model/Command.java
@@ -1,9 +1,13 @@
 package it.unisa.diem.Gruppo20.Model;
 
 /**
- *
+ * This interface represent an action 
+ * that can be performed by calling the execute method.
  * @author Gruppo 20
  */
-public interface Command { 
+public interface Command {
+    /**
+     * Execute this command.
+     */
     void execute();
 }
